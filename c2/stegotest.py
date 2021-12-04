55a67a78a5f22d525cd6fb0cfbc04b96eedb0d83
Jmaxwong <68025573+Jmaxwong@users.noreply.github.com>
2021-12-04 16:38:23 -0500

    finished 2nd and third pixel conversion

--- c2/stegotest.py
+++ c2/stegotest.py
@@ -4,7 +4,7 @@ import random
 
 
 #NOTE**** IT IS (col,row) NOT (row,col)
-def encode(commands, img, iid):  # img is string of file ("diniFall.png") and commands is expected to be arr size 3
+def encodeImage(commands, img, iid):  # img is string of file ("diniFall.png") and commands is expected to be arr size 3
     try:
         img = Image.open(img)
         img_rgb = img.convert("RGB")
@@ -86,27 +86,68 @@ def encode(commands, img, iid):  # img is string of file ("diniFall.png") and co
             oddRGB(1,0,0)
             oddRGB(2,0,0)
             oddRGB(3,0,0)
-            num_commands = 3              
+            num_commands = 3 
+             
 
+        
+        #TODO: make sure offset does not exceed the image size
 
-        offset = random.randint(3, 31)  # endpoints are included
+        offset = random.randint(3, 63)  # endpoints are included #edward shoudnt this be 63??
         # starting pixel position for offset
-        (x, y) = ((offset % width), (int)(offset/width))
-        #col,row
 
-        # TODO: 2nd and third pixel rgb conversion
+
+        while (offset > (width*height)):
+            print("ERROR: INVALID OFFSET GENERATED")
+            print("Offset: " + offset)
+            offset = random.randint(3, 63)     
+
+
+        #TODO: check to see if commands are too big for the image     
+
+        
+        (x, y) = ((offset % width), (int)(offset/width))
+
+        #done TODO: 2nd and third pixel rgb conversion
+        binary_offset = (bin(offset))[2:]
+
+        binary_offset_size = len(binary_offset)
+        if (binary_offset_size < 6):#pad the front with zeros
+            for count in range(binary_offset_size,6):
+                binary_offset = "0"+ binary_offset
+
+
+        for i in range(6):
+            rgb = i%3 + 1
+            if i <= 3:
+                if binary_offset[i] == "1":
+                    oddRGB(rgb,1,0)
+                else:
+                    evenRGB(rgb,1,0)
+            else:
+                if binary_offset[i] == "1":
+                    oddRGB(rgb,2,0)
+                else:
+                    evenRGB(rgb,2,0)
+        
+        print("offset: " + offset)        
+        print("binary_offset: " + binary_offset)
+        print("Double check in new_img pix: ")
+        print(img_rgb.getpixel(0,0))
 
         i = x
         j = y  # this weird code is to make sure it starts at right column and loops correctly
         value = (0,0,0) 
 
-        #TODO: make sure offset does not exceed the image size
+        #TODO: convert char to decimal to binary
+        #ord(num)
+
+
+        
+
 
         while(i < width):
             while(j < height):
-                if(finishedWriting(commands)):
-                    isWriting = False
-                    #TODO: convert char to decimal to binary
+                
 
                 #TODO: add the null byte                 
                 j = j + 1
@@ -125,23 +166,12 @@ def main():
     try:
         img = Image.open("minireeves.png")
         img_rgb = img.convert("RGB")
-        # img.show()
-        # print("test")
-        # print(img.size)
         (x, y) = img.size
         print("width: {}".format(x))
         print("height: {}".format(y))
 
-        # (r,g,b) = img_rgb.getpixel(0,0)
-        # print(r)
-        # print(g)
-        # print(b)
-        # print(img_rgb.getpixel(0,0))
-
         commands = ["yikes","ayy","pizza"]
         empty = ["","",""]
-        print(finishedWriting(commands))
-        print(finishedWriting(empty))
 
         commands[0] = commands[0][1:]
         print(commands[0])
--- c2/testing.py
+++ c2/testing.py
@@ -13,14 +13,33 @@ def main():
     command = "hello xd"
     num_pixels = width * height
     offset = random.randint(2, num_pixels)#endpoints are included
-    offsetpos = ((int)(offset/width), (offset%width)) #pixel position for offset
-    offset_byte = offset.to_bytes(6,"little")
-    print(offset_byte)
+    # offsetpos = ((int)(offset/width), (offset%width)) #pixel position for offset
+    # offset_byte = offset.to_bytes(6,"little")
+    # print(offset_byte)
 
-    print("offset number: {}".format(offset))
-    print("offset byte: {}".format(offset_byte))
-    print(offset_byte.decode('UTF-8'))
+    # print("offset number: {}".format(offset))
+    # print("offset byte: {}".format(offset_byte))
+    # print(offset_byte.decode('UTF-8'))
 
+    binary = bin(2)
+    binary = binary[2:]
+    print("binary: " + binary)
+
+    for j in range(1,7):
+        print(j)
+
+    if (len(binary) < 6):#pad the zeros
+            for i in range(len(binary),6):
+                binary = "0"+ binary
+
+    print(binary)
+
+    iid = 102
+    img_name = "{}_diniFall.jpg".format(iid)
+    print("img_name: " + img_name)
+
+    tester = "I am a test string!"
+    print(tester.encode('ascii'))
 
 
 if __name__ == "__main__":
